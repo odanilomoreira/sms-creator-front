ee7475189e37aa30e3da60d764ba9221f7c06db8
Danilo Moreira <danilokof7@gmail.com>
2021-03-24 19:57:56 -0300

    Card Template almost ready

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Card.css'
 
-function Card({  }) {
-    const charactersCount = () => {
-        // handle characters count
-    }
+function Card({ cardTitle, bodyTitle }) {
+    const [message, setMessage] = useState("")
+    
     return (
         <div className='card'>
-            <div className="card__header"><h1>{cardTitle}</h1>
+            <div className="card__header"><h1>{cardTitle}</h1></div>
                 <div className="card__container"><h3>{bodyTitle}</h3>
-                <div className="card__body"><input type="text">{message}</input></div>
-                <span>{charactersCount}</span>
+                <div className="card__body"><textarea value={message} onChange={e => setMessage(e.target.value)} /></div>
+                <span>Characters: {message.length ? message.length : '0'}</span>
+                
+                <div className="card__add"><strong>Add</strong>
+                <div className="card__addItems">
+                <span>Due Time</span>
+                <span>Resta</span>
+                <span>Due Time</span>
+                </div>
+                </div>
+                <div className="card__buttonsWrapper">
+                    <button className="card__cancelButton">Cancel</button>
+                    <button className="card__saveButton">Save</button>
                 </div>
             </div>
         </div>
--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import './Dashboard.css'
+import Card from './Card'
 
 function Dashboard() {
     return (
         <div className='dashboard'>
-            
+            <div className="dashboard__cardWrapper">
+                <Card 
+                cardTitle="Card Title"
+                bodyTitle="Card Body"
+                />
+            </div>
+            <div className="dashboard__cardWrapper">
+                <Card 
+                cardTitle="Second Card Title"
+                bodyTitle="Second Card Body"
+                />
+            </div>
         </div>
     )
 }
